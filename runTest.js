29d6f584fbb165dce9f23d8958fa107c6b8b5227
Kartik Mehta <77505989+kartikmehta8@users.noreply.github.com>
2025-02-24 18:29:59 +0000

    script to run the tests and send results

---
+++ runTest.js
@@ -0,0 +1,27 @@
+const { exec } = require("child_process");
+const axios = require("axios");
+
+const WEBHOOK_URL = "https://cloud.activepieces.com/api/v1/webhooks/dUmlBBQTPsk2vu6371l6u/sync";
+
+exec("npm test --json --outputFile=test-results.json", async (error, stdout, stderr) => {
+    
+    let testResults = {
+        details: stderr || error?.message || "Unknown error",
+    };
+
+    // DEBUG LOG.
+    // console.log(JSON.stringify(testResults, null, 2));
+
+    // // Prepare payload.
+    const payload = {
+        repository: "AIxBlock-AI-Agent",
+        branch: "main",
+        logs: JSON.stringify(testResults, null, 2)
+    };
+
+    // DEBUG LOG.
+    // console.log(JSON.stringify(payload, null, 2));
+
+    // Send results to ActivePieces Webhook.
+    await axios.post(WEBHOOK_URL, payload);
+});
